ed96805c69ed2c1a5bbf825187febf4362a6a5c0
rwxrob-ghtest2 <robmuh+ghtest2@gmail.com>
2024-05-07 11:42:04 -0400

    remove sum

---
+++ go.mod
@@ -0,0 +1,5 @@
+module rwxrob-ghtest2/go-cmd-with-priv-dep
+
+go 1.22.2
+
+require github.com/rwxrob-ghtestorg2/private-go-pkg v0.0.2
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rwxrob-ghtestorg2/private-go-pkg"
+
+func main() {
+  privpkg.PrintHelloWorld()
+}
